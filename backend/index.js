c3a8c5fa21d172b9ba968dd5f1e9b38d8706dd59
Brian An <b5an@uwaterloo.ca>
2025-04-05 17:34:56 -0400

    socket.io integration for realtime group and API for location and restuarants

---
+++ backend/index.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const dotEnv = require("dotenv").config();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
+  })
+);
+const io = require("socket.io")(server, {
+  cors: {
+    // credentials: true,
+    // origin: "",
+    origin: "*",
+    // methods: ["GET", "POST"],
+    // pingTimeout: 60000,
+    // pingInterval: 120000,
+  },
+});
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const Rooms = require("./rooms");
+
+const { findRoomId } = require("./utilities");
+
+const port = process.env.PORT || 3001;
+
+let rooms = new Rooms();
+
+const getRestaurants = async (location, radius, price) => {
+  try {
+    const yelpRes = await axios.get(process.env.YELP_RESTAURANT_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+      },
+      params: {
+        // location: "Waterloo, Ontario",
+        location: location,
+        radius: radius,
+        price: price,
+        open_now: true,
+        limit: 40,
+      },
+    });
+    return { success: true, restaurants: yelpRes.data.businesses };
+  } catch (error) {
+    console.error(error);
+    return { success: false, restaurants: [] };
+  }
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World! The server is running");
+});
+
+// coordinates are something like this: 43.6532,-79.3832
+app.get("/address/:coordinates", async (req, res) => {
+  try {
+    const [lat, lng] = req.params.coordinates.split(",");
+    const ep = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${process.env.OPEN_CAGE_API_KEY}`;
+    const response = await axios.get(ep);
+    const address = response.data.results[0].formatted;
+    res.json({
+      success: true,
+      address: address,
+    });
+  } catch (e) {
+    console.error(e);
+    res.json({ success: false, error: e });
+  }
+});
+
+app.get("/verify-room/:roomId", async (req, res) => {
+  let { roomId } = req.params;
+  roomId = roomId.toUpperCase();
+  const isValid = rooms.isValidRoomId(roomId);
+  res.json({
+    validRoom: isValid,
+  });
+});
+
+app.post("/create-room", (req, res) => {
+  console.log(`POST to /create-room`);
+  const roomId = rooms.addRoom();
+  res.status(200).json({ roomId });
+});
+
+io.on("connection", (socket) => {
+  console.log("CONNECT", socket.id, "............... NOW");
+  // user joins a room, emit to all users in the room the updated list of all usernames
+  socket.on("JOIN_ROOM", ({ username, roomId, isOwner }) => {
+    console.log(
+      `on JOIN_ROOM: ${username} joined room ${roomId} as ${
+        isOwner ? "owner" : "not owner"
+      }, socket.rooms is: ${JSON.stringify(socket.rooms)}`
+    );
+    roomId = roomId.toUpperCase();
+    socket.join(roomId);
+    rooms.addUserToRoom(username, socket.id, roomId, isOwner);
+
+    const users = rooms.getRoomUsers(roomId);
+    if (!users) {
+      return;
+    }
+    console.log(`emit NEW_ROOM_USERS: ${users.map((user) => user.username)}`);
+    io.in(roomId).emit("NEW_ROOM_USERS", { users: users });
+  });
+
+  // room owner requests the list of restaurants from yelp, emit to all users in the room the list of restaurants
+  socket.on("GET_RESTAURANTS", async ({ location, radius, price }) => {
+    console.log(`on GET_RESTAURANTS: location=${location}`);
+    const room = findRoomId(socket.rooms);
+    const result = await getRestaurants(location, radius, price);
+    io.in(room).emit("FOUND_RESTAURANTS", result);
+  });
+
+  // user accepts a restaurant, if there is a match for all users, emit to all users in the room the updated list of matched restaurants
+  socket.on("ACCEPT_RESTAURANT", ({ restaurantId }) => {
+    console.log(`on ACCEPT_RESTAURANT: restaurantId=${restaurantId}`);
+    const roomId = findRoomId(socket.rooms);
+    const userId = socket.id;
+    const hasMatch = rooms.acceptRestaurant(roomId, userId, restaurantId);
+    // if (hasMatch) {
+    // console.log(`emit MATCHES_FOUND`);
+    // socket.emit("MATCHES_FOUND", rooms.getMatchedRestaurants(roomId));
+    io.in(roomId).emit("MATCHES_FOUND", rooms.getMatchedRestaurants(roomId));
+    // }
+  });
+
+  // user disconnects from room
+  socket.on("disconnecting", () => {
+    console.log("WE ARE NOW DISCONNECTING", socket.id);
+    const roomId = findRoomId(socket.rooms);
+
+    const { hasNewOwner, newOwner } = rooms.removeUserFromRoom(
+      socket.id,
+      roomId
+    );
+    const users = rooms.getRoomUsers(roomId);
+
+    io.in(roomId).emit("NEW_ROOM_USERS", { users: users });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
